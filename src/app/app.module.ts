16616f610807d9a748dc84f2f98187cc64a343ac
FernandoPucci <fsilvapucci@gmail.com>
2017-08-26 12:42:06 -0300

    Add Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,53 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { MyTasksComponent } from './my-tasks/my-tasks.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [  
+  { path: 'newTask',      component: NewTaskComponent },
+  { path: 'myTasks',      component: MyTasksComponent },
+  {
+    path: 'new-task',
+    component: NewTaskComponent,
+    data: { title: 'Nova Tarefa' }
+  },
+  {
+    path: 'my-tasks',
+    component: MyTasksComponent,
+    data: { title: 'Minhas Tarefas' }
+  }
+  ,
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+  // ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewTaskComponent
+    NewTaskComponent,
+    MyTasksComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    //  { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
