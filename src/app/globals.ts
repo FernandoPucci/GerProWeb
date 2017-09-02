301a09100c353a95e33d55efca68a6252c3f8d44
FernandoPucci <fsilvapucci@gmail.com>
2017-09-02 18:14:23 -0300

    Enhancement of checkboxes

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -8,12 +8,12 @@ export const VERSION = '0.0.1';
 export const DEFAULT_COMPANY = 1;
 
 export const DAYS_WEEKS = [
-    { name: 'Domingo', day: 'sunday', check: 'false' },
-    { name: 'Segunda-Feira', day: 'monday', check: 'false' },
-    { name: 'Terça-Feira', day: 'tuesday', check: 'false' },
-    { name: 'Quarta-Feira', day: 'wednesday', check: 'false' },
-    { name: 'Quinta-Feira', day: 'thursday', check: 'false' },
-    { name: 'Sexta-Feira', day: 'friday', check: 'false' },
-    { name: 'Sábado', day: 'saturday', check: 'false' }
+    { name: 'Domingo', day: 'sunday', check: false },
+    { name: 'Segunda-Feira', day: 'monday', check: false },
+    { name: 'Terça-Feira', day: 'tuesday', check: false },
+    { name: 'Quarta-Feira', day: 'wednesday', check: true },
+    { name: 'Quinta-Feira', day: 'thursday', check: false },
+    { name: 'Sexta-Feira', day: 'friday', check: false },
+    { name: 'Sábado', day: 'saturday', check: false }
 ];
 
