9c0ff8f72e7e897d27865272fbb68d529f3bfcc7
FernandoPucci <fsilvapucci@gmail.com>
2018-03-24 13:39:25 -0300

    Fix Navbar issues when logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked } from '@angular/core';
 
 import * as _ from 'lodash';
 import * as globals from './globals';
 import { Router } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { AppService } from './app.service';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
 
   title = globals.APP_TITLE;
   version = globals.VERSION;
+  showBar = false;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private serviceHelper: AppService
+
+  ) {
+    this.checkBar();
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('frur');
+    this.checkBar();
+  }
 
   logout() {
     localStorage.clear();
+    this.checkBar();
     this.router.navigate(['/login']);
+
+  }
+
+  checkBar() {
+    this.showBar = localStorage.getItem(this.serviceHelper.AUTH_COOKIE) != null;
 
   }
 
